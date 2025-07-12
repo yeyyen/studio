febbe8c2b9c50900cef9ec5147e31c411cbd622a
Lorenzo Villafa�e Tabios <lovi.tabios.up@phinmaed.com>
2025-07-12 08:51:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -8,12 +8,16 @@ export function useIsMobile() {
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      setIsMobile(mql.matches)
     }
+    
+    // Set initial value
+    onChange();
+
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
